68b652f249b1dbbf9489db1b752a058f2a9ae350
Bryan Downing <bryan720b@gmail.com>
2019-10-18 12:06:43 -0700

    Building Rough Draft
    Signed-off-by: flyingcoder900 <bryan720b@gmail.com>

--- ReactionCoach.py
+++ ReactionCoach.py
@@ -2,4 +2,28 @@
 ##pip install RPi.GPIO
 #Pull Test
 
+# Create a library of 3 reaction entries to be used.
+
+# program Green LED Ready Light
+
+# Wait for button press from the user
+
+# Press button
+
+
+# Wait a random amount of time that is deemed normal from watching a race and calculating an average.
+
+# If button still pressed then flash light and note time in library place one.
+# If button not pressed then start back at beggining and light green ready light.
+
+# repeat process until all 3
+
+# Calculate the total time range
+
+# If X =< .015 then flash 3 times.
+# If .015 > X >= .025 then flash 2 times.
+# If x > .025 then flash 1 time.
+
+# Reset game
+
 
